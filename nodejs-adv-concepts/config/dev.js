9e1f3302e05713cea69a27e06c4c6fbfb29f3373
Mac Gutierrez <mac.gutierrez@grapikom.com>
2020-05-29 12:02:40 +0800

    Added NodeJs Advance Concepts tutorials

---
+++ nodejs-adv-concepts/config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '406862809864-9boq1kl1sqna2vustg8ev5ir0iccemut.apps.googleusercontent.com',
+  googleClientSecret: 'JrP9OOFTi7PY5y3p1NtTm9WG',
+  mongoURI: 'mongodb://localhost/blog_everyone',
+  cookieKey: '123123123'
+};
