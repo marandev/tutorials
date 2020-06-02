3f55aa52b81fcdf4d031166cac98a6eb3c8d4bb1
Mac Gutierrez <mac.gutierrez@grapikom.com>
2020-06-02 18:31:25 +0800

    Finished automated headless browser test section

--- nodejs-adv-concepts/tests/helpers/page.js
+++ nodejs-adv-concepts/tests/helpers/page.js
@@ -28,12 +28,45 @@ class CustomPage {
 
         await this.setCookie({ name: 'session', value: session });
         await this.setCookie({ name: 'session.sig', value: sig });
-        await this.goto('localhost:3000');
+        await this.goto('localhost:3000/blogs');
         await this.waitFor('a[href="/auth/logout"]');
     }
 
     async getContentsOf(selector) {
         return this.$eval(selector, el => el.innerHTML);
+    }
+
+    get(path) {
+        return this.page.evaluate((_path) => {
+            return fetch(_path, {
+                method: 'GET',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(res => res.json());
+        }, path);
+    }
+
+    post(path, data) {
+        return this.page.evaluate((_path, _data) => {
+            return fetch(_path, {
+                method: 'POST',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(_data)
+            }).then(res => res.json());
+        }, path, data);
+    }
+
+    execRequests(actions) {
+        return Promise.all(
+            actions.map(({ method, path, data}) => {
+                return this[method](path, data);
+            })
+        );
     }
 }
 
