c3b25720d15e98d1ef4ce3b0fa6309a7052e7308
Mac Gutierrez <mac.gutierrez@grapikom.com>
2020-06-08 10:58:41 +0800

    Finish Wiring Up Continuous Integration Section

---
+++ nodejs-adv-concepts/config/ci.js
@@ -0,0 +1,9 @@
+module.exports = {
+    googleClientID:
+      '406862809864-9boq1kl1sqna2vustg8ev5ir0iccemut.apps.googleusercontent.com',
+    googleClientSecret: 'JrP9OOFTi7PY5y3p1NtTm9WG',
+    mongoURI: 'mongodb://localhost/blog_ci',
+    cookieKey: '123123123',
+    redisUrl: 'redis://127.0.0.1:6379'
+  };
+  
--- nodejs-adv-concepts/config/dev.js
+++ nodejs-adv-concepts/config/dev.js
@@ -3,5 +3,6 @@ module.exports = {
     '406862809864-9boq1kl1sqna2vustg8ev5ir0iccemut.apps.googleusercontent.com',
   googleClientSecret: 'JrP9OOFTi7PY5y3p1NtTm9WG',
   mongoURI: 'mongodb://localhost/blog_everyone',
-  cookieKey: '123123123'
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
